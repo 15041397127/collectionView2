8ca70089034f91d165dd793b8c97da941b607cce
ZhangXu <zhangxu@zxdemacbook-pro.local>
2016-07-21 15:07:48 +0800

    Initial Commit

---
+++ collectionView2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionView2
+//
+//  Created by 张旭 on 16/7/21.
+//  Copyright © 2016年 zhangXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ collectionView2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionView2
+//
+//  Created by 张旭 on 16/7/21.
+//  Copyright © 2016年 zhangXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
